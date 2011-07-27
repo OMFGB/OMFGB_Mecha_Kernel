c78268689397d4386fe39a8cac29dc187f56f412
Derek Rodriguez <drod2169@gmail.com>
2011-07-27 13:25:41 -0400

    Remove unnecessary files

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 1 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Sat Jul 23 20:37:31 EDT 2011"
-#define LINUX_COMPILE_TIME "20:37:31"
-#define LINUX_COMPILE_BY "drod2169"
-#define LINUX_COMPILE_HOST "drod2169"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
